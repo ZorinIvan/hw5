f4bb078bc17365bae141e97a1e7b8e3536d9a1de
MichaelBun <micha3008@gmail.com>
2015-06-23 12:14:37 +0300

    added rm

--- HW5/main.cpp
+++ HW5/main.cpp
@@ -328,6 +328,33 @@ int main()
             cerr << "Parse error while parsing " << tokens[0] << endl;
         }
 
+		if (tokens[0] == "rm")
+		{
+			if (tokens.size() != 2) /*Not enough parameters*/
+			{
+				cerr << "Parse error while parsing " << tokens[0] << endl;
+				continue;
+			}
+			else
+			{
+				File* deleted_file = cwd->GetFile(tokens[1]);
+				if (deleted_file == NULL) /*File not found*/
+				{
+					/*Need to print failure*/
+				}
+				Directory* deleted_dir = dynamic_cast<Directory*>(deleted_file);
+				/*Going to check if this is a directory*/
+				if (deleted_dir == NULL) /*Not a directory*/
+				{
+					delete deleted_file;
+				}
+				else /*It is a dir*/
+				{
+
+				}
+			}
+		}
+
         if (tokens[0] == "exit")
         {
         	if(tokens.size()!=1){
